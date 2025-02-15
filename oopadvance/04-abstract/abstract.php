2208489ebd92965788de533e19687835718c1402
Dani170907 <daniramadhan170907@gmail.com>
2025-02-16 01:47:18 +0700

    add comments

--- oopadvance/04-abstract/abstract.php
+++ oopadvance/04-abstract/abstract.php
@@ -1,21 +1,31 @@
 <?php 
 
+// Mendefinisikan abstract class Database
 abstract class Database
 {
+    // Method abstrak connection() yang harus diimplementasikan oleh class turunan
     abstract public function connection();
+
+    // Method konkret disconnect() dengan implementasi default
     public function disconnect()
     {
-        // Implementation
-
+        // Implementation 
+        echo "Disconnect from database \n";
     }
 }
 
+// Mendefinisikan class Model yang mewarisi abstract class Database
 class Model extends Database
 {
+    // Implementasi method connection() yang diwajibkan oleh abstract class Database
     public function connection()
     {
-        //
+        // Implementation
+        echo "Connected from database";
     }
 }
 
+// Membuat objek baru dari class Model
 $model = new Model();
+$model->disconnect();
+$model->connection();
